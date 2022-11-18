819c4042f41e3edc92e3e4fce8fe829419bc273a
kvicksk0up <mixail.sklyar@inbox.ru>
2022-11-18 12:53:27 +0300

    .

--- task_5.py
+++
@@ -1,9 +0,0 @@
-from time import time, localtime
-class Clock:
-
-    @staticmethod
-    def say_time():
-        say_time=localtime(time())
-        return print(f'{say_time.tm_hour}:{say_time.tm_min}:{say_time.tm_sec}')
-
-Clock.say_time()
